6fec8fa32f434257805545718eea011dfe819de0
Lee <xbc1207a@gmail.com>
2016-06-13 16:40:01 +0800

    the function sign up is not finish , so don't merge

---
+++ MemberDB.php
@@ -0,0 +1,138 @@
+<?php
+require_once("DBManager.php");
+class MemberDB extends DBManager{
+	// add one member into member database
+	private function addAMember($account,$password,$userName,$email){
+		$query=$this->db->prepare("insert into member (Account,Password,Username,Email) values(?,?,?,?)");
+		$query->execute( array($account,$password,$userName,$email) );
+	}
+	// this method is to get the user's password
+	public function getPassword($account,$userName){
+		$result=array( "status"=>"","result"=>array() );
+		
+		try{
+			$query=$this->db->perpare("select Password from member where Account=:account and Username=:userName");
+			$query->bindValue(":account",$account);
+			$query->bindValue(":userName",$userName);
+			
+			if( $query->execute() ){
+				$data=$query->fetchAll( PDO::FETCH_ASSOC );
+				
+				if( count($data) ){
+					$result["status"]="warning";
+					$result["status"]["message"]="no data";
+				}
+				else{
+					$result["status"]="Success";
+					$result["result"]["data"]=$data;
+				}
+			}
+			else{
+				$result["status"]="error";
+				$result["status"]["message"]="SQL error";
+			}
+		}
+		catch( PDOException $exception ){
+			$result["status"]="error";
+			$result["status"]["message"]="Exception ".$exception->getMessage();
+		}
+		
+		return json_encode( $result );
+	}
+	public function getUserInformation($account,$password){
+		$result=array( "status"=>"","result"=>array() );
+		
+		try{
+			$query=$this->db->prepare("select * from member where Account=:account and Password=:password");
+			$query->bindValue(":account",$account);
+			$query->bindValue(":password",$password);
+			
+			if( $query->execute() ){
+				$data=$query->fetchAll( PDO::FETCH_ASSOC );
+				
+				if( count( $data )==0 ){
+					$result["status"]="warning";
+					$result["status"]["message"]="no data";
+				}
+				else{
+					$result["status"]="Success";
+					$result["result"]["data"]=$data;
+				}
+			}
+			else{
+				$result["status"]="error";
+				$result["status"]["message"]="SQL error";
+			}
+		}
+		catch( PDOException $exception ){
+			$result["status"]="error";
+			$result["status"]["message"]="Exception ".$exception->getMessage();
+		}
+		
+		return json_encode( $result );
+	}
+	public function signUp($account,$password,$userName,$email){
+		$result=array( "status"=>"","result"=>array() );
+		
+		try{
+			$query=$this->db->prepare("select * from member where Account=:account ");
+			$query->bindValue(":account",$account);
+			
+			if( $query->execute() ){
+				$data=$query->fetchAll( PDO::FETCH_ASSOC );
+				
+				if( count(data)==0 ){
+					$this->addAMember($account,$password,$userName,$email);
+					$result["status"]="Success";
+					$result["data"]="sign up done";
+				}
+				else{
+					$result["status"]="warning";
+					$result["data"]="The account or the password has been used";
+				}
+			}
+			else{
+				$result["status"]="error";
+				$result["status"]["message"]="SQL error";
+			}
+		}
+		catch( PDOException $exception ){
+			$result["status"]="error";
+			$result["status"]["message"]="Exception ".$exception->getMessage();
+		}
+		
+		return json_encode( $result );
+	}
+	public function existThisUser($account,$password){
+		$result=array( "status"=>"","result"=>array() );
+		
+		try{
+			$query=$this->db->prepare("select * from member where Account=:account and Password=:password");
+			$query->bindValue(":account",$account);
+			$query->bindValue(":password",$password);
+			
+			if( $query->execute() ){
+				$data=$query->fetchAll( PDO::FETCH_ASSOC );
+				if( count($data)>0 ){
+					$result["status"]="Success";
+					$result["data"]="true";
+				}
+				else{
+					$result["status"]="Success";
+					$result["data"]="false";
+				}
+			}
+			else{
+				$result["status"]="error";
+				$result["status"]["message"]="SQL error";
+			}
+		}
+		catch( PDOException $exception ){
+			$result["status"]="error";
+			$result["status"]["message"]="Exception ".$exception->getMessage();
+		}
+		
+		return json_encode( $result );
+	}
+}	
+?>
